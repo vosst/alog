bfcce83c71db2a7551b1fce3cc12ff637ddae561
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-10-21 22:32:25 +0200

    add more comprehensive test case covering all known logs

--- logger_reader_test.go
+++ logger_reader_test.go
@@ -2,19 +2,34 @@ package alog
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestReadFromMainWorks(t *testing.T) {
-	if _, err := os.Open("/dev/alog/main"); err != nil {
+func readFromLogWorks(log LogId, t *testing.T) {
+	if _, err := os.Open(filepath.Join("/dev", "alog", log.String())); err != nil {
 		t.Skipf("Android logging facilities are not accessible [%s]", err)
 	}
 
-	lr, err := NewLoggerReader(LogIdMain)
+	lr, err := NewLoggerReader(log)
 	assert.Nil(t, err)
 
-	_, err = lr.ReadNext()
-	assert.Nil(t, err)
+	atLeastOne := false
+
+	for entry, err := lr.ReadNext(); err == nil; entry, err = lr.ReadNext() {
+		atLeastOne = true
+		t.Logf("%+v\n", entry)
+	}
+
+	assert.True(t, atLeastOne)
+
+}
+
+func TestReadFromLogsWorks(t *testing.T) {
+	readFromLogWorks(LogIdMain, t)
+	readFromLogWorks(LogIdRadio, t)
+	readFromLogWorks(LogIdEvents, t)
+	readFromLogWorks(LogIdSystem, t)
 }
