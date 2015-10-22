ddc78846518259b7267a04edf3523a5036943b33
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-10-22 08:04:27 +0200

    Clean up Reader interface

--- reader.go
+++ reader.go
@@ -6,12 +6,9 @@ import (
 )
 
 // ErrReadTimeout is returned if a call to ReadNext times out.
-var ErrReadTimeout = errors.New("Reading the next entry from the log failed")
+var ErrReadTimeout = errors.New("Reading the next entry from the log timed out")
 
 type Reader interface {
-	// SetDeadline adjusts the deadline for all subsequent read operations.
-	SetDeadline(deadline time.Time)
-
 	// ReadNext reads the next entry from a Reader.
 	//
 	// Returns an error if reading the next entry fails.
