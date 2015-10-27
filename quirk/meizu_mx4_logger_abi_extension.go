140326bd1afd4dd53da3e76c2eece3c2cd3a6335
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-10-27 10:28:24 +0100

    Add subdir quirk for handling vendor-specific quirks.

---
+++ quirk/meizu_mx4_logger_abi_extension.go
@@ -0,0 +1,24 @@
+package quirk
+
+// A MeizuMx4LoggerAbiExtension reads the additional timezone field
+// as defined in the kernel source code at:
+//   https://github.com/meizuosc/m75/blob/master/kernel/drivers/staging/android/logger.h.
+type MeizuMx4LoggerAbiExtension struct {
+}
+
+// Prepare is a noop, always returning nil.
+func (self MeizuMx4LoggerAbiExtension) Prepare(fd int) error {
+	return nil
+}
+
+// Read reads the tz field from reader, returning it under key "tz".
+//
+// Returns an error if reading from reader fails.
+func (self MeizuMx4LoggerAbiExtension) Read(reader io.Reader) (map[string]interface{}, error) {
+	tz := int32(-1)
+	if err := binary.Read(reader, binary.LittleEndian, &tz); err != nil {
+		return nil, err
+	}
+
+	return map[string]interface{}{"tz", tz}, nil
+}
