42db6b6e76790a8f70e4da33963100ce9cbe7202
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-11-01 21:31:25 +0100

    Move alog.LoggerReader example into logger_reader_test.go.

--- logger_reader_test.go
+++ logger_reader_test.go
@@ -1,6 +1,7 @@
 package alog
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -73,4 +74,20 @@ func TestLoggerReaderCallsNonNilAbiExtension(t *testing.T) {
 	assert.NoError(t, err)
 
 	mae.AssertNumberOfCalls(t, "Read", 1)
+}
+
+func ExampleLoggerReader() {
+	lr, err := NewLoggerReader(LogIdMain, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Keep on reading for the next 5 seconds.
+	lr.SetDeadline(time.Now().Add(5 * time.Second))
+
+	// Read all available entries from log until an error occurs.
+	// Timing out would return an error here.
+	for le, err := lr.ReadNext(); err == nil; le, err = lr.ReadNext() {
+		fmt.Printf("%s/%s(%d): %s\n", le.Priority, le.Tag, le.Pid, le.Message)
+	}
 }
