78c86f1ac0b857c154788e32d4b4e6a51dbeca17
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-11-02 12:24:35 +0100

    Add convenience functions to LoggerWriter.

--- logger_writer_test.go
+++ logger_writer_test.go
@@ -9,6 +9,13 @@ import (
 
 var testTag = Tag("Test")
 
+func drainLog(reader *LoggerReader) {
+	reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
+	for _, err := reader.ReadNext(); err == nil; _, err = reader.ReadNext() {
+		reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
+	}
+}
+
 func testLoggerWriterWorks(logId LogId, t *testing.T) {
 	skipIfNoAndroidLoggingFacilities(logId, t)
 
@@ -17,10 +24,7 @@ func testLoggerWriterWorks(logId LogId, t *testing.T) {
 
 	defer reader.Close()
 
-	reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
-	for _, err := reader.ReadNext(); err == nil; _, err = reader.ReadNext() {
-		reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
-	}
+	drainLog(reader)
 
 	writer, err := NewLoggerWriter(logId)
 	require.NoError(t, err)
@@ -54,6 +58,53 @@ func TestLoggerWriteWorks(t *testing.T) {
 	testLoggerWriterWorks(LogIdRadio, t)
 	testLoggerWriterWorks(LogIdEvents, t)
 	testLoggerWriterWorks(LogIdSystem, t)
+}
+
+func TestLoggerWriterConvenienceWorks(t *testing.T) {
+	skipIfNoAndroidLoggingFacilities(LogIdMain, t)
+
+	reader, err := NewLoggerReader(LogIdMain, nil)
+	require.NoError(t, err)
+
+	defer reader.Close()
+
+	drainLog(reader)
+
+	writer, err := NewLoggerWriter(LogIdMain)
+	require.NoError(t, err)
+
+	defer writer.Close()
+
+	writer.V(testTag, "V")
+	reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
+	entry, _ := reader.ReadNext()
+	assert.Equal(t, PriorityVerbose, entry.Priority)
+
+	writer.D(testTag, "D")
+	reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
+	entry, _ = reader.ReadNext()
+	assert.Equal(t, PriorityDebug, entry.Priority)
+
+	writer.I(testTag, "I")
+	reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
+	entry, _ = reader.ReadNext()
+	assert.Equal(t, PriorityInfo, entry.Priority)
+
+	writer.W(testTag, "W")
+	reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
+	entry, _ = reader.ReadNext()
+	assert.Equal(t, PriorityWarn, entry.Priority)
+
+	writer.E(testTag, "E")
+	reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
+	entry, _ = reader.ReadNext()
+	assert.Equal(t, PriorityError, entry.Priority)
+
+	writer.F(testTag, "F")
+	reader.SetDeadline(time.Now().Add(500 * time.Millisecond))
+	entry, _ = reader.ReadNext()
+	assert.Equal(t, PriorityFatal, entry.Priority)
+
 }
 
 func ExampleLoggerWriter() {
