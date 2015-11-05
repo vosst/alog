d97b3221b3d96539c3498aace3ab48c616fff9f9
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-11-05 22:27:39 +0100

    Add documentation for package alog.

--- alog.go
+++ alog.go
@@ -1,3 +1,36 @@
+// Package alog enables applications to write to and read from Android's logging facilities.
+//
+// Writing Log Messages
+//
+// Writing to the Android logging facilities can be accomplished in multiple ways.
+// For applications leveraging Go's log package, alog.NewLogger is the way to go:
+//
+// 	logger, err := alog.NewLogger(alog.LogIdMain)
+//	if err != nil {
+//		panic(err)
+//	}
+//	logger.Print("Successfully connected to Android's logging facilities")
+//
+// Convenience functions for all log levels are available and applications can output
+// their tagged messages to Android's well-known logs as in the following example:
+//
+//	alog.D(alog.Main, "tag", "message")
+//
+// Finally, applications can leverage the interface Writer and its implementation LoggerWriter
+// to write to the Android logging facilities. The respective types and functions are meant to
+// be used for integration purposes with other logging frameworks.
+//
+// Reading Log Entries
+//
+// Reading from the Android logging facilities is abstracted by the interface Reader and its
+// implementation LoggerReader. Applications can access Android's well known logs and read individual
+// entries as illustrated in the following snippet:
+//
+// 	lr, err := alog.NewLoggerReader(alog.LogIdMain)
+//	lr.SetDeadline(time.Now().Add(500 * time.Millisecond))
+//	for entry, err := lr.ReadNext(); err == nil; entry, err = lr.ReadNext() {
+//		lr.SetDeadline(time.Now().Add(500 * time.Millisecond))
+//	}
 package alog
 
 import "log"
